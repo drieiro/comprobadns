1b79fdff72d4abb8e51c2de3497d0c7ebdd5cf4d
drieiro <daniel@danielrieiro.com>
2022-08-16 19:37:29 +0200

    Build arm64

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #/bin/sh
-env GOOS=linux GOARCH=amd64 go build -o ../bin
-env GOOS=windows GOARCH=amd64 go build -o ../bin
+env GOOS=linux GOARCH=amd64 go build -o ../bin/chkdns-linux
+env GOOS=windows GOARCH=amd64 go build -o ../bin/chkdns-win.exe
+env GOOS=linux GOARCH=arm64 go build -o ../bin/chkdns-arm64
 chown $1:$2 ../bin/*
